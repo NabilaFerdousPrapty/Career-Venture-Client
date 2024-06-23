8d388e3442ef551ffa0dee87c6eed675cd484e96
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-23 21:41:06 +0600

    hook added

---
+++ src/hooks/UseTheme/UseTheme.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const UseTheme = ({children}) => {
+    const [theme, setTheme] = useState('light');
+    
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UseTheme;
