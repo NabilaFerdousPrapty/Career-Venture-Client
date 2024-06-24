e1da6e1d6e14e9b4f3ae1ce45315ca13d2019f21
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-24 22:33:40 +0600

    navbar almost done

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -32,10 +32,11 @@ function Navbar() {
                     {/* Mobile Menu open: "block", Menu closed: "hidden" */}
                     <div className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out  lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center ${isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full'}`}>
                         <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Join Slack</a>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Browse Topics</a>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Random Item</a>
-                            <a href="#" className="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700">Experts</a>
+                          <Link to="/" className="px-6 py-2 text-lg font-semibold text-gray-700 dark:text-gray-200">Home</Link>
+                            <Link to="/employment" className="px-6 py-2 text-lg font-semibold text-gray-700 dark:text-gray-200">Employments</Link>
+                            <Link to="/bootCamps" className="px-6 py-2 text-lg font-semibold text-gray-700 dark:text-gray-200">Bootcamps</Link>
+                            <Link to="/mentors" className="px-6 py-2 text-lg font-semibold text-gray-700 dark:text-gray-200">Mentors</Link>
+                            <Link to="/dashBoard" className="px-6 py-2 text-lg font-semibold text-gray-700 dark:text-gray-200">Dashboard</Link>
                         </div>
 
                         <div className="flex items-center mt-4 lg:mt-0">
