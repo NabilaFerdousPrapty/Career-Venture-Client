1f0340a39b14cf910cce636fc7185ed49623c90a
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-25 16:53:19 +0600

    banner almost done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,18 +3,16 @@ import Home from "../Pages/Home/Home";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
-
 const MainLayout = () => {
-    return (
-        <div>
-           <div className="max-w-7xl mx-auto">
-           <Navbar />
-         <Outlet/>
-           </div>
-            <Footer />
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -29,16 +29,16 @@ const Banner = () => {
           <div
             className="rounded-md h-screen"
             style={{
-              backgroundImage: `url('https://i.ibb.co/PGxL15n/525-REp-WIE1-BUi-Aw-MTkt-MTE.jpg'),
+              backgroundImage: `url('https://i.ibb.co/QX06fch/laptop-coffee-cup-wooden-table-front-window-with-city-view.jpg'),
               linear-gradient(to right, rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0))`,
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-purple-100 p-6 rounded-2xl">
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-purple-100 p-6 rounded-2xl">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-                Welcome to Track Tonic
+               
               </h1>
               <p className="lg:text-xl text-sm">
                 Get your fitness on with our classes
@@ -50,7 +50,7 @@ const Banner = () => {
               >
                 All Classes
               </Link>
-            </div>
+            </div> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -58,13 +58,13 @@ const Banner = () => {
             className="rounded-md h-screen "
             style={{
               position: "relative",
-              backgroundImage: `url('https://i.ibb.co/y47GN6T/freepik-hand-drawn-fitness-we-are-offline-twitch-background-2024060315492399a-T.png')`,
+              backgroundImage: `url('https://i.ibb.co/XWb9Bdn/high-angle-desktop-with-laptop-copy-space.jpg')`,
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-gray-800 rounded-full p-10 bg-cyan-100 rounded-2xl">
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-gray-800 rounded-full p-10 bg-cyan-100 rounded-2xl">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
                 Start your fitness journey
               </h1>
@@ -78,7 +78,7 @@ const Banner = () => {
               >
                 All classes
               </Link>
-            </div>
+            </div> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -86,13 +86,13 @@ const Banner = () => {
             className="rounded-md h-screen"
             style={{
               position: "relative",
-              backgroundImage: `url('https://i.ibb.co/VJ0H1B6/9455415.jpg')`,
+              backgroundImage: `url('https://i.ibb.co/fnGp5Vx/personal-accessories-coffee-cup-earphone-eyeglasses-keyboard-black-background.jpg')`,
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-amber-200 rounded-full p-10 ">
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-amber-200 rounded-full p-10 ">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
                 We are here to help you
               </h1>
@@ -106,7 +106,7 @@ const Banner = () => {
               >
                 All classes
               </Link>
-            </div>
+            </div> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -114,13 +114,13 @@ const Banner = () => {
             className="rounded-md h-screen"
             style={{
               position: "relative",
-              backgroundImage: `url('https://i.ibb.co/vc6TYvv/7799147.jpg')`,
+              backgroundImage: `url('https://i.ibb.co/Wyws7nX/flat-lay-desktop-with-agenda-magnifying-glass.jpg')`,
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
             }}
           >
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-sky-200 p-10 rounded-full">
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-sky-200 p-10 rounded-full">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
                 Welcome to Track Tonic
               </h1>
@@ -134,7 +134,35 @@ const Banner = () => {
               >
                 All classes
               </Link>
-            </div>
+            </div> */}
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="rounded-md h-screen"
+            style={{
+              position: "relative",
+              backgroundImage: `url('https://i.ibb.co/k6FfPsF/laptop-eyeglasses-mouse-diary-pen-pot-plant-black-background.jpg')`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-sky-200 p-10 rounded-full">
+              <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
+                Welcome to Track Tonic
+              </h1>
+              <p className="lg:text-xl text-sm">
+                Get your fitness on with our classes
+              </p>
+
+              <Link
+                to="/allClasses"
+                className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
+              >
+                All classes
+              </Link>
+            </div> */}
           </div>
         </SwiperSlide>
       </Swiper>
