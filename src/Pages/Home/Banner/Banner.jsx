1198a5079143b41a3af21fb48bc7978c609cae37
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-25 17:19:28 +0600

    banner done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -30,32 +30,33 @@ const Banner = () => {
             className="rounded-md h-screen"
             style={{
               backgroundImage: `url('https://i.ibb.co/QX06fch/laptop-coffee-cup-wooden-table-front-window-with-city-view.jpg'),
-              linear-gradient(to right, rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0))`,
+      linear-gradient(to right, rgba(20, 0, 0, 0.5), rgba(0, 0, 0, 0))`,
               backgroundPosition: "center",
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
             }}
           >
-            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-purple-100 p-6 rounded-2xl">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-transparent p-6 rounded-2xl border-2 border-amber-700 text-gray-700  ">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-               
+                Discover Your Dream Career
               </h1>
               <p className="lg:text-xl text-sm">
-                Get your fitness on with our classes
+                Explore thousands of job opportunities tailored to your skills
+                and interests.
               </p>
-
               <Link
-                to="/allClasses"
+                to="/bootCamps"
                 className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
               >
-                All Classes
+                Get Started
               </Link>
-            </div> */}
+            </div>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div
-            className="rounded-md h-screen "
+            className="rounded-md h-screen"
             style={{
               position: "relative",
               backgroundImage: `url('https://i.ibb.co/XWb9Bdn/high-angle-desktop-with-laptop-copy-space.jpg')`,
@@ -64,23 +65,24 @@ const Banner = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-gray-800 rounded-full p-10 bg-cyan-100 rounded-2xl">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-transparent p-6 rounded-2xl border-2 border-amber-300">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-                Start your fitness journey
+                Upskill for the Future
               </h1>
               <p className="lg:text-xl text-sm">
-                Get your fitness on with our classes
+                Enroll in top-rated courses and training programs to enhance
+                your skills.
               </p>
-
               <Link
-                to="/allClasses"
+                to="/bootCamps"
                 className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
               >
-                All classes
+                Learn More
               </Link>
-            </div> */}
+            </div>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div
             className="rounded-md h-screen"
@@ -92,23 +94,24 @@ const Banner = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-amber-200 rounded-full p-10 ">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-transparent p-6 rounded-2xl border-2 border-amber-400">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-                We are here to help you
+                Connect with Experts
               </h1>
               <p className="lg:text-xl text-sm">
-                Get your fitness on with our classes
+                Get guidance from industry professionals to accelerate your
+                career growth.
               </p>
-
               <Link
-                to="/allClasses"
+                to="/bootCamps"
                 className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
               >
-                All classes
+                Find Mentors
               </Link>
-            </div> */}
+            </div>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div
             className="rounded-md h-screen"
@@ -120,23 +123,24 @@ const Banner = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-sky-200 p-10 rounded-full">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-transparent p-6 rounded-2xl border-2 border-amber-400">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-                Welcome to Track Tonic
+                Personalized Career Paths
               </h1>
               <p className="lg:text-xl text-sm">
-                Get your fitness on with our classes
+                Discover career paths and job opportunities that match your
+                unique talents.
               </p>
-
               <Link
-                to="/allClasses"
+                to="/bootCamps"
                 className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
               >
-                All classes
+                Explore Now
               </Link>
-            </div> */}
+            </div>
           </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <div
             className="rounded-md h-screen"
@@ -148,21 +152,21 @@ const Banner = () => {
               backgroundRepeat: "no-repeat",
             }}
           >
-            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-sky-200 p-10 rounded-full">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center bg-transparent p-6 rounded-2xl border-2 border-amber-400">
               <h1 className="lg:text-4xl text-2xl font-bold lg:my-5">
-                Welcome to Track Tonic
+                Land Your Dream Job
               </h1>
               <p className="lg:text-xl text-sm">
-                Get your fitness on with our classes
+                Apply for jobs at top companies and take the next step in your
+                career.
               </p>
-
               <Link
-                to="/allClasses"
+                to="/bootCamps"
                 className="bg-white text-black px-4 py-2 rounded-md mt-4 lg:mt-8 inline-block"
               >
-                All classes
+                Apply Now
               </Link>
-            </div> */}
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
