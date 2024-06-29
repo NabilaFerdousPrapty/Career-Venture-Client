780f4623e2d2910e667904decec5673c7e3a60e3
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-06-29 21:24:08 +0600

    sign up page design done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -11,9 +11,9 @@ const SignUp = () => {
     
       const onSubmit = (data) => console.log(data)
   return (
-    <div>
-      <section className="bg-white dark:bg-gray-900">
-    <div className="flex justify-center min-h-screen max-w-5xl">
+    <div className=" flex justify-between items-center">
+    
+    <div className="max-w-5xl w-screen flex justify-center h-screen  mx-auto py-6 rounded-2xl  shadow-lg bg-[#1c2940]">
         <div className="hidden bg-cover lg:block lg:w-2/5" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}
         >
         </div>
@@ -28,53 +28,27 @@ const SignUp = () => {
                     Let’s get you all set up so you can verify your personal account and begin setting up your profile.
                 </p>
 
-                <div className="mt-6">
-                    <h1 className="text-gray-500 dark:text-gray-300">Select type of account</h1>
-
-                    <div className="mt-3 md:flex md:items-center md:-mx-2">
-                        <button className="flex justify-center w-full px-6 py-3 text-white bg-blue-500 rounded-lg md:w-auto md:mx-2 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                            </svg>
-
-                            <span className="mx-2">
-                                client
-                            </span>
-                        </button>
-
-                        <button className="flex justify-center w-full px-6 py-3 mt-4 text-blue-500 border border-blue-500 rounded-lg md:mt-0 md:w-auto md:mx-2 dark:border-blue-400 dark:text-blue-400 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
-
-                            <span className="mx-2">
-                                worker
-                            </span>
-                        </button>
-                    </div>
-                </div>
+              
 
                 <form className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
-                    <div>
-                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">First Name</label>
+                    <div className="col-span-2">
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200"> Name</label>
                         <input type="text" placeholder="John" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                     </div>
-
-                    <div>
-                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Last name</label>
-                        <input type="text" placeholder="Snow" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-
-                    <div>
-                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Phone number</label>
-                        <input type="text" placeholder="XXX-XX-XXXX-XXX" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-
-                    <div>
+                    <div className="col-span-2">
                         <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
                         <input type="email" placeholder="johnsnow@example.com" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                     </div>
 
+                    <div className="col-span-2">
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Photo</label>
+                        <input type="file"  className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                  
+
+                   
+                   
                     <div>
                         <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
                         <input type="password" placeholder="Enter your password" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
@@ -99,7 +73,7 @@ const SignUp = () => {
             </div>
         </div>
     </div>
-</section>
+
     </div>
   );
 };
