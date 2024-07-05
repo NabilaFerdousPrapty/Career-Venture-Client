3206ccc67455fa7178cfaa6aeb5c1102c3adc9c7
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-07-05 22:58:22 +0600

    loading spinner added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,16 +2,41 @@ import { Outlet } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
+import { useEffect, useState } from "react";
+import { CircleLoader, RingLoader } from "react-spinners";
 
 const MainLayout = () => {
+  const [loading, setLoading] = useState(true);
+  console.log(loading);
+ useEffect(() => {
+   const timer = setTimeout(() => {
+     setLoading(false);
+   }, 1500); 
+
+   return () => clearTimeout(timer);
+ }, []);
+    
+
   return (
     <div>
+      {loading ? (
+        <div className="h-screen  flex justify-center items-center">
+        <CircleLoader className="text-5xl"  color={"#d19945"} loading={loading} size={150} />
+        </div>
+        
+      ) :(
+        <div>
+          
       <div className="max-w-7xl mx-auto">
         <Navbar />
         <Outlet />
       </div>
       <Footer />
+      </div>
+      )}
+    
     </div>
+
   );
 };
 
