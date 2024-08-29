41bc7810061993a91f9ac989e86aa00faf86a81e
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-08-30 02:24:35 +0600

    bootcamps added

--- src/Pages/BootCamps/BootCamps.jsx
+++ src/Pages/BootCamps/BootCamps.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-
+import React from "react";
+import UseAxiosCommon from "../../hooks/UseAxiosCommon/UseAxiosCommon";
+import { useQuery } from "@tanstack/react-query";
 const BootCamps = () => {
-    return (
-        <div>
-           <h1>
-            Bootcamps
-            </h1> 
-        </div>
-    );
+  const axiosCommon = UseAxiosCommon();
+  const { data: bootCamps = [], isLoading } = useQuery({
+    queryKey: ["bootCamps"],
+    queryFn: async () => {
+      const { data } = await axiosCommon.get("/bootCamps");
+      return data;
+    },
+  });
+  return (
+    <div>
+     
+    </div>
+  );
 };
 
 export default BootCamps;
