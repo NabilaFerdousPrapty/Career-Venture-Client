8747fc885152ab4eb9102347258d9b3a3a961380
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-09-16 19:19:03 +0600

    user management on going

--- src/Pages/Admin/UserManagement/UserManagement.jsx
+++ src/Pages/Admin/UserManagement/UserManagement.jsx
@@ -21,9 +21,111 @@ const UserManagement = () => {
       console.log("Error:", error);
       console.log("Data:", users);
     return (
-        <div>
-            
+      <div className="text-[#98BDE6]">
+      <h1 className="text-3xl font-bold my-5 text-center">User Management</h1>
+      {!users && isLoading && (
+        <div className="flex justify-center items-center">
+          <HashLoader />
         </div>
+      )}
+      <section className="container px-4 mx-auto">
+        <div className="flex flex-col">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden xl:max-w-5xl lg:max-w-3xl md:max-w-2xl overflow-x-scroll max-w-sm  border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Index
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Phone Number
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Balance
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Status
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        User Type
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Authorize
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    {users &&
+                      users.map((user, index) => (
+                        <tr key={user.id}>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            {index + 1}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            {user.name}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            {user.email}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                           
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                         
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            {user.status}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            {user.AppliedAs}
+                          </td>
+                          <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-400">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                              Authorize
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
     );
 };
 
